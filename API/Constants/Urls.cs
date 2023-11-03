33709a2150f1e06017206865f502ec9ff63a44ca
OmarMohamed256 <omarmohamed2342@outlook.com>
2023-11-04 00:05:20 +0200

    change to local host

--- API/Constants/Urls.cs
+++ API/Constants/Urls.cs
@@ -2,6 +2,6 @@ namespace API.Constants
 {
     public static class Urls
     {
-        public const string BaseUrl = "http://3.75.235.56:80/";
+        public const string BaseUrl = "http://localhost:5170/";
     }
 }
